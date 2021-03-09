9a95063806103115882685c062da5a4250dd399b
Bartek3b <73472410+Bartek3b@users.noreply.github.com>
2021-03-09 11:23:55 +0100

    sticky navbar

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,23 @@
 const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
 
 window.addEventListener('scroll', function(){
     if(window.screen.width<512){
         if(this.pageYOffset>100){
             header.classList.add("header--down");
             header.classList.remove("header");
-        }   else{
+            footer.remove();
+        }else{
             header.classList.add("header");
             header.classList.remove("header--down");
         }
     }
+});
 
+window.addEventListener("resize", function(){
+    if(window.screen.width>512){
+        header.classList.add("header");
+        header.classList.remove("header--down");
+        document.body.appendChild(footer);
+    }
 });
